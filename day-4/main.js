c9189e9be156933507106001b41e18aaceaea100
Daria Pronina <pronina.dm@gmail.com>
2023-12-04 20:12:41 +0200

    day 4 part 1

---
+++ day-4/main.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+const inputData = fs.readFileSync('input.txt', 'utf8').split('\n');
+
+const inputData1 = [
+  'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53',
+  'Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19',
+  'Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1',
+  'Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83',
+  'Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36',
+  'Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11',
+]
+
+const getCardNumber = (str) => {
+  const [, number] = str.split(' ');
+  return +number;
+}
+
+const getNumbers = (str) => {
+  return str.trim().split(' ').filter(n => !!n).map(n => +n)
+}
+
+let sum = 0;
+
+inputData.forEach(cardStr => {
+  const [cardPart, valuesPart] = cardStr.split(':');
+  const cardNumber = getCardNumber(cardPart);
+  const [winningNumbers, cardNumbers] = valuesPart.split('|').map(p => getNumbers(p));
+
+  const cardPoints = winningNumbers.length + cardNumbers.length - new Set([...winningNumbers, ...cardNumbers]).size;
+
+  if (cardPoints > 0) {
+    sum += Math.pow(2, cardPoints - 1);
+  }
+})
+
+console.log(sum)
