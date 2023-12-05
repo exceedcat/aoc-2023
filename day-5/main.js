cbdf9147fd986947981b1bd2f2c85525a9ef4ce9
Daria Pronina <pronina.dm@gmail.com>
2023-12-05 23:36:36 +0200

    day 5 part 2 attempt lol

--- day-5/main.js
+++ day-5/main.js
@@ -20,20 +20,6 @@ const getMap = (type) => {
   return map;
 }
 
-const getValue = (map, source) => {
-  const possibleRangeStart = Object.keys(map).sort((a, b) => a - b).findLast(key => +key <= +source);
-  if (possibleRangeStart === undefined) {
-    return source;
-  }
-
-  const {destinationStart, length} = map[possibleRangeStart];
-  if (+possibleRangeStart + +length < +source) {
-    return source;
-  }
-  return +destinationStart + (+source - +possibleRangeStart);
-
-}
-
 const seeds = parseNumbersLine(inputData[0].split(': ')[1]);
 
 const maps = [
@@ -46,16 +32,64 @@ const maps = [
   'humidity-to-location'
 ].map(getMap);
 
-const values = []
 
-for (let i = 0; i < seeds.length; i++) {
-  let temp = seeds[i];
+const valuesMap = {};
+const values1 = []
 
-  maps.forEach(map => {
-    temp = getValue(map, temp);
-  })
 
-  values.push(temp);
+const getValue1 = (map, source) => {
+  if (map[source]) {
+    return { value: map[source].destinationStart, final: map[source].final};
+  }
+  const possibleRangeStart = Object.keys(map).sort((a, b) => a - b).findLast(key => +key <= +source && map[key].length);
+  if (possibleRangeStart === undefined) {
+    return { value: source };
+  }
+
+  const {destinationStart, length} = map[possibleRangeStart];
+  if (+possibleRangeStart + +length < +source) {
+    return { value: source };
+  }
+  return { value: +destinationStart + (+source - +possibleRangeStart) };
+
 }
 
-console.log(Math.min(...values))
+for (let i = 0; i < seeds.length; i += 2) {
+  for (let seed = +seeds[i]; seed < +seeds[i] + +seeds[i+1]; seed++) {
+    // console.log('> process', seed)
+    if (valuesMap[seed]) {
+      values1.push(valuesMap[seed]);
+      console.log('cache hit')
+      continue;
+    }
+    const allMapsAccessed = [];
+    let temp = seed;
+
+    for (let m = 0; m < maps.length; m++) {
+      if (maps[m][temp]?.final) {
+        temp = maps[m][temp].final;
+        console.log('cache hit')
+        break;
+      }
+      const { value, final } = getValue1(maps[m], temp);
+      if (!maps[m][temp]) {
+        maps[m][temp] = {}
+      }
+      allMapsAccessed.push(maps[m][temp])
+      if (final) {
+        temp = final;
+        console.log('cache hit')
+        break;
+      } else {
+        temp = value;
+      }
+    }
+
+    allMapsAccessed.forEach(map => { map.final = temp })
+
+    valuesMap[seed] = temp;
+    values1.push(temp);
+  }
+}
+
+console.log(Math.min(...values1))
