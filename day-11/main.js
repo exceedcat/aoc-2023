be6f1d3dff58d43b44e352cbcf5b2b13986fc276
Daria Pronina <pronina.dm@gmail.com>
2023-12-11 21:42:45 +0200

    day 11 refactor

--- day-11/main.js
+++ day-11/main.js
@@ -2,8 +2,7 @@ const fs = require('fs');
 
 const inputData = fs.readFileSync('input.txt', 'utf8').split('\n');
 
-const universe = inputData.map(str => str.split(''));
-
+const EXPAND = 1000000;
 const getGalaxies = u => {
   const coords = [];
   for (let i = 0; i < u.length; i++) {
@@ -13,9 +12,6 @@ const getGalaxies = u => {
   }
   return coords;
 }
-
-const galaxies = getGalaxies(universe);
-
 const getGalaxyPairs = g => {
   return g.reduce((res, coords, index, g) => {
     let pairs = [];
@@ -25,43 +21,39 @@ const getGalaxyPairs = g => {
     return [...res, ...pairs];
   }, [])
 }
+const getRowsAndColsWithoutGalaxies = u => {
+  const rowsWithoutGalaxies = [];
+  for (let i = 0; i < u.length; i++) {
+    if (u[i].indexOf('#') === -1) rowsWithoutGalaxies.push(i);
+  }
 
-const galaxyPairs = getGalaxyPairs(galaxies);
-
-console.log(galaxyPairs.length)
-
-const rowsWithoutGalaxies = [];
-for (let i = 0; i < universe.length; i++) {
-  if (universe[i].indexOf('#') === -1) rowsWithoutGalaxies.push(i);
-}
-
-const columnsWithoutGalaxies = [];
-for (let i = 0; i < universe[0].length; i++) {
-  let without = true;
-  for (let j = 0; j < universe.length; j++) {
-    if (universe[j][i] === '#') {
-      without = false;
-      break;
+  const columnsWithoutGalaxies = [];
+  for (let i = 0; i < u[0].length; i++) {
+    let without = true;
+    for (let j = 0; j < u.length; j++) {
+      if (u[j][i] === '#') {
+        without = false;
+        break;
+      }
+    }
+    if (without) {
+      columnsWithoutGalaxies.push(i);
     }
   }
-  if (without) {
-    columnsWithoutGalaxies.push(i);
-  }
+
+  return { rowsWithoutGalaxies, columnsWithoutGalaxies }
 }
-
-const EXPAND = 1000000;
-
-const getLongPath = ([a,b]) => {
+const getPath = ([a, b]) => {
   const emptyColumns = columnsWithoutGalaxies.filter(c => (c < a[1] && c > b[1]) || (c > a[1] && c < b[1]));
-  if (a[0] === b[0]) {
-    return Math.abs(a[1] - b[1]) + emptyColumns.length * (EXPAND - 1);
-  }
   const emptyRows = rowsWithoutGalaxies.filter(c => (c < a[0] && c > b[0]) || (c > a[0] && c < b[0]));
-  if (a[1] === b[1]) {
-    return Math.abs(a[0] - b[0]) + emptyRows.length * (EXPAND - 1);
-  }
+
   return Math.abs(a[1] - b[1]) + Math.abs(a[0] - b[0]) + emptyColumns.length * (EXPAND - 1) + emptyRows.length * (EXPAND - 1);
 }
-const longPaths = galaxyPairs.map(getLongPath);
 
-console.log(longPaths.reduce((res, el) => res + el, 0))
+const universe = inputData.map(str => str.split(''));
+const galaxies = getGalaxies(universe);
+const galaxyPairs = getGalaxyPairs(galaxies);
+const { rowsWithoutGalaxies, columnsWithoutGalaxies } = getRowsAndColsWithoutGalaxies(universe)
+const paths = galaxyPairs.map(getPath);
+
+console.log(paths.reduce((res, el) => res + el, 0))
